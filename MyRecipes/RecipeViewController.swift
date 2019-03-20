3027ef840bb4b34cb3efdb1951eafcad26eccfdf
Wiktoria <wiktoria@marcins-macbook-air.local>
2019-03-20 14:21:49 +0100

    dodanie dodawania zdjecia z biblioteki telefonu

--- MyRecipes/RecipeViewController.swift
+++ MyRecipes/RecipeViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import os.log
 
-class RecipeViewController: UIViewController {
+class RecipeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var nameRecipeTextField: UITextField!
     @IBOutlet weak var ingredientsTable: UITableView!
@@ -21,6 +21,29 @@ class RecipeViewController: UIViewController {
     
     var recipe: Recipe?
 
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        descriptionTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        imageRecipe.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
@@ -33,7 +56,7 @@ class RecipeViewController: UIViewController {
         let name = nameRecipeTextField.text!
         let ingredients = ["maslo", "olej"]
         let descriptions = descriptionTextField.text!
-        let image = UIImage(named: "image1")!
+        let image = imageRecipe.image!
         
         recipe = Recipe.init(name: name, ingredients: ingredients, description: descriptions, image: image)
         
